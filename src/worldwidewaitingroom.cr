6b0ce532da573f502eb9889e60eaaebdaf4577ee
Frankie Primerano <frankieprimerano@gmail.com>
2024-10-07 02:30:10 -0400

    new rules.

--- src/worldwidewaitingroom.cr
+++ src/worldwidewaitingroom.cr
@@ -8,33 +8,39 @@ alias Secret = String
 alias Public = String
 
 templates = Templates.new
-redis = Redis::PooledClient.new
-sockets = Hash(HTTP::WebSocket, Tuple(Secret, Public)).new
 
-def update (sockets, redis, templates)
-  seen = Set(Secret).new
-  puts "Game loop tick... Num sockets #{sockets.size}"
+module WWWR
+  R = Redis::PooledClient.new
+  Online = Hash(Public, Secret).new
+end
 
-  tick_global_timer redis
-  tick_offline_leaderboard redis
+def update
+  puts "Game loop tick... Num sockets #{WWWR::Online.size}"
 
-  sockets.each_value do |pub_priv|
-    priv_key, pub_key = pub_priv
+  tick_global_timer
 
-    if !seen.includes? priv_key
-      seen.add priv_key
-      add_time_to redis, pub_key, 1
-      update_leaderboard_for redis, pub_key
+  get_leaderboard.each do |data|
+    pub_key = data["user"]
+    if WWWR::Online.has_key? pub_key
+      add_time_to pub_key, 1
+    else
+      inc_key("offline_time", pub_key)
     end
+
+    if (get_int_key "rate_limit", pub_key) > 0
+      dec_key "rate_limit", pub_key
+    end
+
+    update_leaderboard_for pub_key
   end
 end
 
+
 # Global timer
 spawn do
-  redis.del("leaderboard")
   loop do
     begin
-      update sockets, redis, templates
+      update
     rescue ex
       puts "Exception in main loop #{ex}"
     end
@@ -43,75 +49,52 @@ spawn do
   end
 end
 
-def tick_global_timer (r)
-  gtl = get_global_time_left r
-  r.incrby "global_time", -1
+def tick_global_timer
+  gtl = get_global_time_left
+  WWWR::R.incrby "global_time", -1
   if gtl <= 0
-    reset_game r
+    reset_game
   end
 end
 
-def tick_offline_leaderboard (r)
-  (get_offline_leaderboard r).each do |data|
-    public_key = data["user"]
-    time_waited = get_wait_time r, public_key
+def reset_game
+  WWWR::R.set "global_time", 604800
 
-    puts "Offline leaderboard #{public_key}"
+  leaderboard = get_leaderboard
+  WWWR::R.lpush "leaderboards", leaderboard.to_json
 
-    if time_waited <= 0
-      r.zrem "offline_leaderboard", public_key
-      next
-    end
-
-    offline_time_waited = (get_offline_time r, public_key)
-    if time_waited > offline_time_waited
-      r.hincrby "offline_time_waited", public_key, 1
-    end
-    r.zadd "offline_leaderboard", (offline_time_waited + 1), public_key
-  end
-end
-
-def reset_game (r)
-  r.set "global_time", 604800
-
-  leaderboard = get_leaderboard r
-  r.lpush "leaderboards", leaderboard.to_json
-  r.del "offline_leaderboard"
-
-  time_waited_keys = r.hkeys("time_waited")
+  time_waited_keys = WWWR::R.hkeys("time_waited")
 
   time_waited_keys.each do |key|
-    r.hset("time_waited", key, 0)
+    WWWR::R.hset("time_waited", key, 0)
   end
 end
 
-def get_global_time_left (r)
-  global_time = r.get "global_time"
+def get_global_time_left
+  global_time = WWWR::R.get "global_time"
 
   if global_time == nil
-    r.set "global_time", 604800
+    WWWR::R.set "global_time", 604800
   end
 
   global_time ||= 604800
   global_time.to_i
 end
 
-def add_time_to (r, pubkey, seconds)
-  r.hincrby("time_waited", pubkey, seconds)
+def add_time_to (pubkey, seconds)
+  WWWR::R.hincrby("time_waited", pubkey, seconds)
 end
 
-def remove_time_from (r, pubkey, seconds)
-  add_time_to r, pubkey, -seconds
+def remove_time_from (pubkey, seconds)
+  add_time_to pubkey, -seconds
 end
 
-def update_leaderboard_for (r, public_key)
-  r.zadd("leaderboard", (get_wait_time r, public_key), public_key)
-  r.zrem("offline_leaderboard", public_key)
-  r.hset("offline_time_waited", public_key, 0)
+def update_leaderboard_for (public_key)
+  WWWR::R.zadd("leaderboard", (get_wait_time public_key), public_key)
 end
 
-def get_data_for (r, pub_key)
-  mdata = r.hget "data", pub_key
+def get_data_for (pub_key)
+  mdata = WWWR::R.hget "data", pub_key
 
   if !mdata
     return nil
@@ -123,46 +106,39 @@ def get_data_for (r, pub_key)
     return nil
   end
 
-  parsed["time_waited"] = build_time_left_string (get_wait_time r, pub_key)
-  parsed["offline_time_waited"] = build_time_left_string (get_offline_time r, pub_key)
+  parsed["time_waited"] = build_time_left_string (get_wait_time pub_key)
+  parsed["offline_time"] = build_time_left_string (get_offline_time pub_key)
   parsed["user"] = "#{pub_key}"
+  parsed["is_online"] = (WWWR::Online.has_key? pub_key)
+  parsed["rate_limit_time_left"] = build_time_left_string (get_rate_limit pub_key)
 
   parsed
 end
 
-def set_data_for (r, pub_key, data : String)
-  r.hset "data", pub_key, data
+def set_data_for (pub_key, data : String)
+  WWWR::R.hset "data", pub_key, data
 end
 
-def hydrate_pub_key_set (r, key)
-  leaderboard = r.zrange(key, 0, -1).reverse
-  data = [] of Hash(String, String | Int64 | Bool)
-
-  leaderboard.each do |member|
-    mdat = get_data_for r, member
-    if mdat
-      data << mdat
-    end
-  end
-
-  data
-
-end
-
-def get_leaderboard_place (r, pub_key)
-  leaderboard = r.zrange("leaderboard", 0, -1).reverse
+def get_leaderboard_place (pub_key)
+  leaderboard = WWWR::R.zrange("leaderboard", 0, -1).reverse
 
   place = leaderboard.index pub_key
   place ||= 10000000
   place + 1
 end
 
-def get_leaderboard (r)
-  hydrate_pub_key_set r, "leaderboard"
-end
+def get_leaderboard
+  leaderboard = WWWR::R.zrange("leaderboard", 0, -1).reverse
+  data = [] of Hash(String, String | Int64 | Bool)
 
-def get_offline_leaderboard (r)
-  hydrate_pub_key_set r, "offline_leaderboard"
+  leaderboard.each do |member|
+    mdat = get_data_for member
+    if mdat
+      data << mdat
+    end
+  end
+
+  data
 end
 
 def append_time_to_string (str, value, unit)
@@ -192,82 +168,103 @@ def build_time_left_string (seconds)
     output
 end
 
-def setup_new_waiter (r)
+def setup_new_waiter
   puts "Setting up new waiter."
   secret_token = Random.new.hex
   public_token = Random.new.hex
 
-  r.hset("tokens", secret_token, public_token)
-  r.hset("time_waited", public_token, 0)
-  r.hset("data", public_token, { "name" => "Anonymous", "color" => "#ffffff", "compressed" => false }.to_json)
+  WWWR::R.hset("tokens", secret_token, public_token)
+  WWWR::R.hset("time_waited", public_token, 0)
+  WWWR::R.hset("data", public_token, { "name" => "Anonymous", "color" => "#ffffff", "compressed" => false }.to_json)
 
   secret_token
 end
 
-def secret_to_public (r, secret_key)
-  return r.hget "tokens", secret_key
+def secret_to_public (secret_key)
+  WWWR::R.hget "tokens", secret_key
 end
 
-def get_wait_time (r, public_key) : Int64
-  time_waited = r.hget("time_waited", public_key)
-  time_waited ||= 0
-  Int64.new time_waited.to_i
+def get_int_key (hmap, key) : Int64
+  value = WWWR::R.hget(hmap, key)
+  value ||= 0
+  Int64.new value.to_i
 end
 
-def get_offline_time (r, public_key) : Int64
-  offline_time = r.hget("offline_time_waited", public_key)
-  offline_time ||= 0
-  Int64.new offline_time.to_i
+def inc_key (hmap, key)
+  WWWR::R.hincrby hmap, key, 1
 end
 
-def can_take (r, pub_key) : Bool
+def dec_key (hmap, key)
+  WWWR::R.hincrby hmap, key, -1
+end
+
+def get_offline_time (pub_key)
+  get_int_key "offline_time", pub_key
+end
+
+def get_wait_time (public_key)
+  get_int_key "time_waited", public_key
+end
+
+def get_rate_limit (pub_key)
+  get_int_key "rate_limit", pub_key
+end
+
+def can_take (pub_key) : Bool
   if !pub_key
     return false
   end
 
-  can_take = r.get ("exp" + pub_key)
+  can_take = get_int_key "rate_limit", pub_key
 
-  return can_take == nil
+  return can_take <= 0
 end
 
-def rate_limit_take (r, pub_key)
+def rate_limit_take (pub_key)
   if !pub_key
     return
   end
 
-  r.set ("exp" + pub_key), "yes"
-  r.expire ("exp" + pub_key), 60 * 60 * 24
+  WWWR::R.hset "rate_limit", pub_key, 60 * 60 * 24
 end
 
 post "/transfer" do |ctx|
   secret_key = ctx.request.cookies["token"].value
-  public_key = secret_to_public redis, secret_key
+  public_key = secret_to_public secret_key
 
   waiter = ctx.params.body["waiter"].as String
   action = ctx.params.body["action"].as String
 
   puts "#{waiter} #{action} #{public_key}"
 
-  amount = get_offline_time redis, waiter
+  amount = get_offline_time waiter
 
   puts "#{public_key} #{waiter}"
+  is_waiter_online = WWWR::Online.has_key? waiter
 
-  if public_key != waiter
+  puts "WAITER ONLINE #{is_waiter_online}"
+
+  if is_waiter_online
+    puts "#{public_key} tried to take from #{waiter} but he was online."
+    next "Can't take from an online player."
+  end
+
+  if public_key != waiter && !is_waiter_online
      if action == "take"
-      if !(can_take redis, public_key)
+      if !(can_take public_key)
         puts "#{public_key} was rate limited."
         next "No"
       end
 
-      waiter_wait_time = (get_wait_time redis, waiter)
+      waiter_wait_time = (get_wait_time waiter)
 
       puts "#{waiter_wait_time} #{amount}"
 
       if waiter_wait_time >= amount
         puts "#{public_key} took offline time #{waiter_wait_time} from #{waiter}"
-        remove_time_from redis, waiter, amount
-        add_time_to redis, public_key, amount
-        rate_limit_take redis, public_key
+        remove_time_from waiter, amount
+        add_time_to public_key, amount
+        rate_limit_take public_key
       end
     end
   end
@@ -277,44 +274,45 @@ before_all do |ctx|
   begin
     secret_key = ctx.request.cookies["token"].value
 
-    if !(secret_to_public redis, secret_key)
-      secret_key = setup_new_waiter redis
+    if !(secret_to_public secret_key)
+      secret_key = setup_new_waiter
     end
 
   rescue
-    secret_key = setup_new_waiter redis
+    secret_key = setup_new_waiter
   end
 
+  puts "Setup new waiter"
   ctx.request.cookies["token"] = secret_key
   ctx.response.cookies["token"] = secret_key
 end
 
-def get_pub_key_from_ctx (r, ctx)
+def get_pub_key_from_ctx (ctx)
   secret_key = ctx.request.cookies["token"].value
-  public_key = secret_to_public r, secret_key
+  public_key = secret_to_public secret_key
   public_key
 end
 
 get "/" do |ctx|
-  public_key = get_pub_key_from_ctx redis, ctx
-  templates.render "index.html", get_data_for redis, public_key
+  public_key = get_pub_key_from_ctx ctx
+  templates.render "index.html", get_data_for public_key
 end
 
 post "/compressed" do |ctx|
-  public_key = get_pub_key_from_ctx redis, ctx
+  public_key = get_pub_key_from_ctx ctx
   compressed = ctx.params.body["compressed"].as String
-  data = get_data_for redis, public_key
+  data = get_data_for public_key
   if data
     data["compressed"] = compressed == "yes"
-    set_data_for redis, public_key, data.to_json
+    set_data_for public_key, data.to_json
   end
 
   templates.render "compress-button.html", { "compressed" => compressed == "yes" }
 end
 
 get "/compressed" do |ctx|
-  public_key = get_pub_key_from_ctx redis, ctx
-  data = get_data_for redis, public_key
+  public_key = get_pub_key_from_ctx ctx
+  data = get_data_for public_key
 
   if data
     is_compressed = data.fetch "compressed", false
@@ -325,68 +323,62 @@ get "/compressed" do |ctx|
   templates.render "compress-button.html", { "compressed" => is_compressed }
 end
 
-def remove_from_leaderboard (r, pub_key)
-  puts "Removed #{pub_key} from leaderboard"
-  r.zrem("leaderboard", pub_key)
-  r.zadd("offline_leaderboard", 0, pub_key)
-end
-
 post "/name" do |ctx|
-  public_key = get_pub_key_from_ctx redis, ctx
+  public_key = get_pub_key_from_ctx ctx
   name = ctx.params.body["name"].as String
-  data = get_data_for redis, public_key
+  data = get_data_for public_key
   if data
     data["name"] = name
-    set_data_for redis, public_key, data.to_json
+    set_data_for public_key, data.to_json
   end
 end
 
 post "/color" do |ctx|
-  public_key = get_pub_key_from_ctx redis, ctx
+  public_key = get_pub_key_from_ctx ctx
   color = ctx.params.body["color"].as String
-  data = get_data_for redis, public_key
+  data = get_data_for public_key
   if data && /\#[0-9a-f]{6}/.match color
     data["color"] = color
-    set_data_for redis, public_key, data.to_json
+    set_data_for public_key, data.to_json
   end
 end
 
 ws "/ws" do |socket, context|
   secret_key = context.request.cookies["token"].value
-  pub_key = secret_to_public redis, secret_key
+  pub_key = secret_to_public secret_key
 
   if pub_key
     puts "#{secret_key} connected."
-    update_leaderboard_for redis, pub_key
-    sockets[socket] = ({ secret_key, pub_key })
+    update_leaderboard_for pub_key
+    WWWR::R.hdel("offline_time", pub_key)
+    WWWR::Online[pub_key] = secret_key
   end
 
   socket.on_message do
-    leaderboard = get_leaderboard redis
-    offline_leaderboard = get_offline_leaderboard redis
-    global_time = build_time_left_string (get_global_time_left redis)
+    leaderboard = get_leaderboard
+    global_time = build_time_left_string (get_global_time_left)
 
-    data = get_data_for redis, pub_key
+    data = get_data_for pub_key
 
     if !data
       puts "No data from Redis in render loop.... skipping."
       next
     end
 
-    data["place"] = get_leaderboard_place redis, pub_key
+    data["place"] = get_leaderboard_place pub_key
 
     html = templates.render "live-html.html", {
-        "leaderboard" => leaderboard,
-        "offline_leaderboard" => offline_leaderboard,
-        "this_waiter" => pub_key,
-        "data" => data,
-        "can_take" => (can_take redis, pub_key), "time_left" => global_time
+      "leaderboard" => leaderboard,
+      "this_waiter" => pub_key,
+      "is_online" => (WWWR::Online.has_key? pub_key),
+      "data" => data,
+      "can_take" => (can_take pub_key),
+      "time_left" => global_time
     }
 
     if socket.closed?
       puts "Socket is closed. Ignoring"
-      sockets.delete socket
-      remove_from_leaderboard redis, pub_key
+      WWWR::Online.delete pub_key
       next
     end
 
@@ -394,18 +386,13 @@ ws "/ws" do |socket, context|
       socket.send html
     rescue
       puts "Could not send to socket. Ignoring."
-      sockets.delete socket
-      remove_from_leaderboard redis, pub_key
+      WWWR::Online.delete pub_key
     end
   end
 
   socket.on_close do
     puts "Socket closed"
-    if pub_key
-      redis.zrem("leaderboard", pub_key)
-      sockets.delete socket
-      remove_from_leaderboard redis, pub_key
-    end
+    WWWR::Online.delete pub_key
   end
 end
 
