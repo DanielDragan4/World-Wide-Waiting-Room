ef525b276e4b6a22b93596dc6ad93cbd62fe1f53
Frankie Primerano <frankieprimerano@gmail.com>
2024-10-13 02:18:48 -0400

    core engine rewrite

---
+++ public/worker.js
@@ -0,0 +1,53 @@
+let players = {}
+
+
+// Based on how fast the update loop ticks
+const NGU_SCALE_FACTOR = 1 / 100
+
+// Update Loop
+setInterval(() => {
+  Object.keys(players).forEach((public_key) => {
+    intervalFunc(public_key)
+  })
+}, 10)
+
+// Render Loop
+setInterval(() => {
+  const leaderboard = Object.values(players).sort((a, b) => a.time_units <= b.time_units ? 1 : -1)
+  postMessage(leaderboard)
+}, 10)
+
+function intervalFunc(public_key) {
+  const this_player = players[public_key]
+  const tps = Number(this_player.time_units_per_second) * NGU_SCALE_FACTOR
+  const tu = Number(this_player.time_units)
+
+  const next_tu = tu + tps;
+
+  players[public_key] = {
+    ...players[public_key],
+    time_units: next_tu
+  }
+}
+
+function syncProcedure(e) {
+  console.log("Syncing", e);
+  players = {}
+
+  const { leaderboard } = e
+
+  leaderboard.forEach((p) => {
+    players[p.public_key] = {
+      ...p
+    }
+  })
+}
+
+onmessage = ({ data: jsonMsg }) => {
+  const { event } = jsonMsg;
+  switch (event) {
+    case "sync": {
+      syncProcedure(jsonMsg)
+    }
+  }
+}
--- src/worldwidewaitingroom.cr
+++ src/worldwidewaitingroom.cr
@@ -1,5 +1,4 @@
 require "kemal"
-require "lz4"
 require "random"
 require "json"
 require "redis"
@@ -14,469 +13,326 @@ templates = Templates.new
 module WWWR
   R = Redis::PooledClient.new
   Online = Hash(Public, Secret).new
-  Leaderboard = Array(Hash(String, String | Int64 | Bool)).new
-  Place = Hash(Public, Int32).new
+  Channels = Hash(String, Channel(Nil)).new
 end
 
-def update
-  puts "Game loop tick... Num sockets #{WWWR::Online.size}"
+module Events
+  SYNC = "sync"
+  LOGIN = "login"
+  ORDER_CHANGED = "order_change"
+  LOGOUT = "logout"
+  PLAYER_UPDATE = "player_update"
 
-  tick_global_timer
-  update_leaderboard
+  def self.sync (leaderboard, player_data)
+    {
+      "event" => Events::SYNC,
+      "leaderboard" => leaderboard,
+      "player" => player_data
+    }
+  end
+end
 
-  WWWR::Leaderboard.each do |data|
-    pub_key = data["user"]
-    if WWWR::Online.has_key? pub_key
-      add_time_to pub_key, 1
+module Keys
+  GAME_END_TIME = "end_time"
+  COOKIE = "token"
+  LAST_FRAME_TIME = "frame_time"
+  LEADERBOARD = "online-leaderboard"
+  PLAYER_TOKENS = "tokens"
+  PLAYER_TIME_UNITS = "time_units"
+  PLAYER_TIME_UNITS_PER_SECOND = "time_units_per_second"
+  PLAYER_POWERUPS = "powerups"
+  PLAYER_NAME = "name"
+  PLAYER_TEXT_COLOR = "bg_color"
+  PLAYER_BG_COLOR = "text_color"
+end
+
+class Game
+  @sync_next_frame = false
+
+  def spawn_loop
+    update_frame_time
+    spawn do
+      loop do
+        tick
+        sleep 1.second
+      end
+    end
+  end
+
+  def tick
+    dt = frame_dt_ms
+    puts "Last frame #{dt}ms"
+
+    multiplier = dt / 1000.0
+
+    WWWR::R.pipelined do |r|
+      get_leaderboard.each do |player_data|
+        player_public_key = player_data["public_key"]
+        player_tups = get_player_time_units_ps player_public_key
+        inc_time_units player_public_key, player_tups * multiplier
+      end
+    end
+
+    if @sync_next_frame
+      sync
+      @sync_next_frame = false
+    end
+
+    update_frame_time
+  end
+
+  def update_frame_time
+    WWWR::R.set(Keys::LAST_FRAME_TIME, Time.utc.to_unix_ms)
+  end
+
+  def get_leaderboard
+    leaderboard = WWWR::R.zrange(Keys::LEADERBOARD, 0, -1)
+    leaderboard.map { |public_key| get_data_for public_key }
+  end
+
+  def inc_time_units (public_key, by)
+    player_tu = get_player_time_units public_key
+    updated_tu = player_tu + by
+
+    WWWR::R.pipelined do |r|
+      r.hset Keys::PLAYER_TIME_UNITS, public_key, updated_tu
+      r.zadd Keys::LEADERBOARD, updated_tu, public_key
+    end
+  end
+
+  def add_to_leaderboard (public_key)
+    WWWR::R.zadd Keys::LEADERBOARD, (get_player_time_units public_key), public_key
+  end
+
+  def remove_from_leaderboard (public_key)
+    WWWR::R.zrem Keys::LEADERBOARD, public_key
+  end
+
+  def sync
+    WWWR::Channels.each_value do |c|
+      spawn do
+        next if c.closed?
+        c.send nil
+      end
+    end
+  end
+
+  def defer_sync
+    @sync_next_frame = true
+  end
+
+  def broadcast_online (public_key)
+    add_to_leaderboard public_key
+    puts "Broadcase online from #{public_key}"
+    sync
+  end
+
+  def broadcast_offline (public_key)
+    remove_from_leaderboard public_key
+    puts "Broadcase offline from #{public_key}"
+    sync
+  end
+
+  def get_agent_powerups (public_key)
+    WWWR::R.zrange("powerups-#{public_key}", 0, -1)
+  end
+
+  def setup_new_waiter
+    puts "Setting up new waiter."
+    secret_token = Random.new.hex
+    public_key = Random.new.hex
+
+    WWWR::R.pipelined do |r|
+      r.hset(Keys::PLAYER_TOKENS, secret_token, public_key)
+      r.hset(Keys::PLAYER_TIME_UNITS, public_key, 0)
+      r.hset(Keys::PLAYER_NAME, public_key, "Anonymous")
+      r.hset(Keys::PLAYER_BG_COLOR, public_key, "#ffffff")
+      r.hset(Keys::PLAYER_TEXT_COLOR, public_key, "#000000")
+      r.hset(Keys::PLAYER_TIME_UNITS_PER_SECOND, public_key, 1)
+    end
+
+    secret_token
+  end
+
+  def get_data_for (public_key)
+    time_units = Redis::Future.new
+    player_name = Redis::Future.new
+    player_bg_color = Redis::Future.new
+    player_text_color = Redis::Future.new
+    tps = Redis::Future.new
+
+    WWWR::R.pipelined do |r|
+      time_units = r.hget(Keys::PLAYER_TIME_UNITS, public_key)
+      player_name = r.hget(Keys::PLAYER_NAME, public_key)
+      player_bg_color = r.hget(Keys::PLAYER_BG_COLOR, public_key)
+      player_text_color = r.hget(Keys::PLAYER_TEXT_COLOR, public_key)
+      tps = r.hget(Keys::PLAYER_TIME_UNITS_PER_SECOND, public_key)
+    end
+
+    return {
+      "name" => player_name.value,
+      "bg_color" => player_bg_color.value,
+      "text_color" => player_text_color.value,
+      "time_units" => time_units.value.to_s.to_f64?,
+      "time_units_per_second" => tps.value.to_s.to_f64?,
+      "public_key" => public_key
+    }
+  end
+
+  def set_bg_color_for (public_key, color)
+    WWWR::R.hset(Keys::PLAYER_BG_COLOR, public_key, color)
+  end
+
+  def set_text_color_for (public_key, color)
+    WWWR::R.hset(Keys::PLAYER_TEXT_COLOR, public_key, color)
+  end
+
+  def set_name_for (public_key, name)
+    WWWR::R.hset(Keys::PLAYER_NAME, public_key, name)
+  end
+
+  def frame_dt_ms
+    now = Time.utc.to_unix_ms
+    last_frame_time = WWWR::R.get(Keys::LAST_FRAME_TIME)
+    last_frame_time ||= now
+    now - last_frame_time.to_i64
+  end
+
+  def reset_game
+
+  end
+
+  def update_for (public_key)
+    data = get_data_for public_key
+  end
+
+  def secret_to_public (secret_key)
+    WWWR::R.hget Keys::PLAYER_TOKENS, secret_key
+  end
+
+  def get_player_time_units (public_key)
+    result = WWWR::R.hget Keys::PLAYER_TIME_UNITS, public_key
+    if result
+      return result.to_f64
     else
-      inc_key("offline_time", pub_key)
-    end
-
-    if (get_int_key "rate_limit", pub_key) > 0
-      dec_key "rate_limit", pub_key
-    end
-
-    if (get_int_key "rate_limit_steal", pub_key) > 0
-      dec_key "rate_limit_steal", pub_key
-    end
-
-    update_leaderboard_for pub_key
-  end
-end
-
-
-# Global timer
-spawn do
-  loop do
-    begin
-      update
-    rescue ex
-      puts "Exception in main loop #{ex}"
-    end
-
-    sleep 1
-  end
-end
-
-def tick_global_timer
-  gtl = get_global_time_left
-  WWWR::R.incrby "global_time", -1
-  if gtl <= 0
-    reset_game
-  end
-end
-
-def reset_game
-  WWWR::R.set "global_time", 604800
-
-  leaderboard = get_leaderboard
-  WWWR::R.lpush "leaderboards", leaderboard.to_json
-
-  time_waited_keys = WWWR::R.hkeys("time_waited")
-
-  time_waited_keys.each do |key|
-    WWWR::R.hset("time_waited", key, 0)
-  end
-end
-
-def get_global_time_left
-  global_time = WWWR::R.get "global_time"
-
-  if global_time == nil
-    WWWR::R.set "global_time", 604800
-  end
-
-  global_time ||= 604800
-  global_time.to_i
-end
-
-def add_time_to (pubkey, seconds)
-  WWWR::R.hincrby("time_waited", pubkey, seconds)
-end
-
-def remove_time_from (pubkey, seconds)
-  add_time_to pubkey, -seconds
-end
-
-def update_leaderboard_for (public_key)
-  WWWR::R.zadd("leaderboard", (get_wait_time public_key), public_key)
-end
-
-def get_data_for (pub_key)
-  hydrate_data pub_key, (WWWR::R.hget "data", pub_key)
-end
-
-def hydrate_data (pub_key, mdata)
-  if !mdata
-    return nil
-  end
-
-  begin
-    parsed = Hash(String, String | Int64 | Bool).from_json(mdata)
-  rescue
-    return nil
-  end
-
-  takeable_time = (get_takeable_time pub_key)
-  wait_time = (get_wait_time pub_key)
-
-  parsed["raw_wait_time"] = wait_time
-  parsed["time_waited"] = build_time_left_string wait_time
-  parsed["offline_time"] = build_time_left_string (get_offline_time pub_key)
-  parsed["has_time_to_take"] = takeable_time > 0
-  parsed["takeable_time"] = build_time_left_string takeable_time
-  parsed["user"] = "#{pub_key}"
-  parsed["is_online"] = (WWWR::Online.has_key? pub_key)
-  parsed["rate_limit_time_left"] = build_time_left_string (get_rate_limit pub_key)
-
-  parsed
-end
-
-def set_data_for (pub_key, data : String)
-  WWWR::R.hset "data", pub_key, data
-end
-
-def get_leaderboard_place (pub_key)
-  WWWR::Place.fetch pub_key, 100000
-end
-
-def update_leaderboard
-  WWWR::Leaderboard.clear
-  WWWR::Place.clear
-
-  leaderboard = WWWR::R.zrange("leaderboard", 0, -1).reverse
-  fetched_lb = [] of Redis::Future
-
-  WWWR::R.pipelined do |p|
-     leaderboard.each do |member|
-      fetched_lb << p.hget "data", member
+      return Float64.new 0.0
     end
   end
 
-  fetched_lb.each_with_index do |m, i|
-    next if !m || !m.value
-
-    pk = leaderboard.fetch i, nil
-
-    next if !pk
-
-    WWWR::Place[pk.as String] = i + 1
-    hd = (hydrate_data pk.as String, m.value.as String)
-
-    next if !hd
-
-    WWWR::Leaderboard << hd
-  end
-end
-
-def get_leaderboard
-  WWWR::Leaderboard
-end
-
-def append_time_to_string (str, value, unit)
-  if value > 0
-    str = "#{str}#{value} #{unit}"
-    if value > 1
-      str = "#{str}s"
-    end
-    str = "#{str} "
-  end
-  str
-end
-
-def build_time_left_string (seconds)
-    days_left =    seconds // 60 // 60 // 24
-    hours_left =   seconds // 60 // 60 % 24
-    minutes_left = seconds // 60 % 60
-    seconds_left = seconds % 60
-
-    output = ""
-
-    output = append_time_to_string output, days_left, "day"
-    output = append_time_to_string output, hours_left, "hour"
-    output = append_time_to_string output, minutes_left, "minute"
-    output = append_time_to_string output, seconds_left, "second"
-
-    output
-end
-
-def setup_new_waiter
-  puts "Setting up new waiter."
-  secret_token = Random.new.hex
-  public_token = Random.new.hex
-
-  WWWR::R.hset("tokens", secret_token, public_token)
-  WWWR::R.hset("time_waited", public_token, 0)
-  WWWR::R.hset("data", public_token, { "name" => "Anonymous", "color" => "#ffffff", "compressed" => false }.to_json)
-
-  secret_token
-end
-
-def secret_to_public (secret_key)
-  WWWR::R.hget "tokens", secret_key
-end
-
-def get_int_key (hmap, key) : Int64
-  value = WWWR::R.hget(hmap, key)
-  value ||= 0
-  Int64.new value.to_i
-end
-
-def inc_key (hmap, key)
-  WWWR::R.hincrby hmap, key, 1
-end
-
-def dec_key (hmap, key)
-  WWWR::R.hincrby hmap, key, -1
-end
-
-def get_offline_time (pub_key)
-  get_int_key "offline_time", pub_key
-end
-
-def get_wait_time (public_key)
-  get_int_key "time_waited", public_key
-end
-
-def get_rate_limit (pub_key)
-  get_int_key "rate_limit", pub_key
-end
-
-def get_rate_limit_steal (pub_key)
-  get_int_key "rate_limit_steal", pub_key
-end
-
-def can_take (pub_key) : Bool
-  return false if !pub_key
-
-  can_take = get_int_key "rate_limit", pub_key
-
-  can_take <= 0
-end
-
-def can_steal (pub_key) : Bool
-  return false if !pub_key || (get_wait_time pub_key) < 2
-
-  can_steal = get_int_key "rate_limit_steal", pub_key
-
-  can_steal <= 0
-end
-
-def rate_limit_take (pub_key)
-  return if !pub_key
-
-  WWWR::R.hset "rate_limit", pub_key, 60 * 60 * 24
-end
-
-def rate_limit_steal (pub_key)
-  return if !pub_key
-
-  WWWR::R.hset "rate_limit_steal", pub_key, 1
-end
-
-def get_takeable_time (pub_key)
-  Math.min (get_offline_time pub_key), (get_wait_time pub_key)
-end
-
-post "/transfer" do |ctx|
-  secret_key = ctx.request.cookies["token"].value
-  public_key = secret_to_public secret_key
-
-  waiter = ctx.params.body["waiter"].as String
-  action = ctx.params.body["action"].as String
-
-  puts "#{waiter} #{action} #{public_key}"
-
-  amount = get_takeable_time waiter
-
-  puts "#{public_key} #{waiter}"
-
-  is_waiter_online = WWWR::Online.has_key? waiter
-
-  if public_key != waiter
-    puts "ACTION TRIED #{action}"
-     case action
-       when "take"
-
-        puts "WAITER ONLINE #{is_waiter_online}"
-
-        if is_waiter_online
-          puts "#{public_key} tried to take from #{waiter} but he was online."
-          next "Can't take from an online player."
-        end
-
-        if !(can_take public_key)
-          puts "#{public_key} was rate limited on take."
-          next "No"
-        end
-
-        puts "#{public_key} took offline time #{amount} from #{waiter}"
-        remove_time_from waiter, amount
-        add_time_to public_key, amount
-        rate_limit_take public_key
-      when "steal"
-        if !is_waiter_online
-          puts "Steal tried for someone not online."
-          next "No"
-        end
-
-        if (!can_steal public_key)
-          puts "#{public_key} was rate limited on steal."
-          next "No"
-        end
-
-        if (get_wait_time waiter) >= 10
-          puts "#{public_key} stole 10s from #{waiter}"
-          remove_time_from waiter, 10
-          add_time_to public_key, 10
-          rate_limit_steal public_key
-        else
-          puts "#{waiter} doesn't have enough time to steal."
-        end
-      when "give"
-        if !is_waiter_online
-          puts "Give tried for someone not online."
-          next "No"
-        end
-
-        if (get_wait_time public_key) < 10
-          puts "You don't have enough time to give."
-        else
-          puts "#{public_key} gave 10s to #{waiter}"
-          remove_time_from public_key, 10
-          add_time_to waiter, 10
-        end
+  def get_player_time_units_ps (public_key)
+    result = WWWR::R.hget Keys::PLAYER_TIME_UNITS_PER_SECOND, public_key
+    if result
+      return result.to_f64
+    else
+      return Float64.new 0.0
     end
   end
+
+  def get_public_key_from_ctx (ctx)
+    secret_key = ctx.request.cookies[Keys::COOKIE].value
+    public_key = secret_to_public secret_key
+    public_key
+  end
 end
+
+game = Game.new
+game.spawn_loop
 
 before_all do |ctx|
   begin
-    secret_key = ctx.request.cookies["token"].value
+    secret_key = ctx.request.cookies[Keys::COOKIE].value
 
-    if !(secret_to_public secret_key)
-      secret_key = setup_new_waiter
+    if !(game.secret_to_public secret_key)
+      secret_key = game.setup_new_waiter
     end
 
   rescue
-    secret_key = setup_new_waiter
+    secret_key = game.setup_new_waiter
   end
 
   puts "Setup new waiter"
-  ctx.request.cookies["token"] = secret_key
-  ctx.response.cookies["token"] = secret_key
-end
+  cookie = HTTP::Cookie.new Keys::COOKIE, secret_key
+  cookie.max_age = (10 * 364 * 24 * 60 * 60).seconds
+  cookie.path = "/"
 
-def get_pub_key_from_ctx (ctx)
-  secret_key = ctx.request.cookies["token"].value
-  public_key = secret_to_public secret_key
-  public_key
+  ctx.request.cookies <<  cookie
+  ctx.response.cookies << cookie
 end
 
 get "/" do |ctx|
-  public_key = get_pub_key_from_ctx ctx
-  templates.render "index.html", get_data_for public_key
-end
-
-post "/compressed" do |ctx|
-  public_key = get_pub_key_from_ctx ctx
-  compressed = ctx.params.body["compressed"].as String
-  data = get_data_for public_key
-  if data
-    data["compressed"] = compressed == "yes"
-    set_data_for public_key, data.to_json
-  end
-
-  templates.render "compress-button.html", { "compressed" => compressed == "yes" }
-end
-
-get "/compressed" do |ctx|
-  public_key = get_pub_key_from_ctx ctx
-  data = get_data_for public_key
-
-  if data
-    is_compressed = data.fetch "compressed", false
-  else
-    is_compressed = false
-  end
-
-  templates.render "compress-button.html", { "compressed" => is_compressed }
+  public_key = game.get_public_key_from_ctx ctx
+  templates.render "index.html", game.get_data_for public_key
 end
 
 post "/name" do |ctx|
-  public_key = get_pub_key_from_ctx ctx
+  public_key = game.get_public_key_from_ctx ctx
   name = ctx.params.body["name"].as String
-  data = get_data_for public_key
-  if data
-    data["name"] = name
-    set_data_for public_key, data.to_json
+  if name && public_key
+    game.set_name_for public_key, name
+    game.sync
   end
 end
 
 post "/color" do |ctx|
-  public_key = get_pub_key_from_ctx ctx
-  color = ctx.params.body["color"].as String
-  data = get_data_for public_key
-  if data && /\#[0-9a-f]{6}/.match color
-    data["color"] = color
-    set_data_for public_key, data.to_json
+  public_key = game.get_public_key_from_ctx ctx
+
+  bg_color = ctx.params.body.fetch "bg", ""
+  text_color = ctx.params.body.fetch "text", ""
+
+  regex = /\#[0-9a-f]{6}/
+
+  if public_key
+    if bg_color && regex.match bg_color
+      game.set_bg_color_for public_key, bg_color
+      game.sync
+    elsif text_color && regex.match text_color
+      game.set_text_color_for public_key, text_color
+      game.sync
+    end
   end
 end
 
 ws "/ws" do |socket, context|
   secret_key = context.request.cookies["token"].value
-  pub_key = secret_to_public secret_key
+  public_key = game.secret_to_public secret_key
+  channel_key = Random.new.hex
 
-  if pub_key
+  events = Channel(Nil).new
+
+  spawn do
+    loop do
+      if events.closed?
+        break
+      end
+
+      select
+      when events.receive?
+        sync_data = Events.sync game.get_leaderboard, game.get_data_for public_key
+        socket.send sync_data.to_json
+      else
+      end
+
+      Fiber.yield
+    end
+  end
+
+  if public_key
     puts "#{secret_key} connected."
-    update_leaderboard_for pub_key
-    WWWR::R.hdel("offline_time", pub_key)
-    WWWR::Online[pub_key] = secret_key
+    WWWR::Online[public_key] = secret_key
+    WWWR::Channels[channel_key] = events
+    game.broadcast_online public_key
   end
 
   socket.on_message do
-    leaderboard = get_leaderboard
-    global_time = build_time_left_string (get_global_time_left)
-
-    data = get_data_for pub_key
-
-    if !data
-      puts "No data from Redis in render loop.... skipping."
-      next
-    end
-
-    data["place"] = get_leaderboard_place pub_key
-
-    html = templates.render "live-html.html", {
-      "leaderboard" => leaderboard,
-      "this_waiter" => pub_key,
-      "is_online" => (WWWR::Online.has_key? pub_key),
-      "data" => data,
-      "can_take" => (can_take pub_key),
-      "can_steal" => (can_steal pub_key),
-      "time_left" => global_time
-    }
-
-    #compressed_html = Base64.urlsafe_encode Compress::LZ4.encode(html), padding: true
-    compressed_html = Compress::LZ4.encode(html)
-
-    if socket.closed?
-      puts "Socket is closed. Ignoring"
-      WWWR::Online.delete pub_key
-      next
-    end
-
-    begin
-      socket.send compressed_html
-    rescue
-      puts "Could not send to socket. Ignoring."
-      WWWR::Online.delete pub_key
-    end
+    events.send nil
   end
 
   socket.on_close do
     puts "Socket closed"
-    WWWR::Online.delete pub_key
+    WWWR::Online.delete public_key
+    WWWR::Channels.delete channel_key
+    events.close
+    game.broadcast_offline public_key
   end
 end
 
