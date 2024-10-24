2797c8aa9f4d57eaf4c2934aac6578aedca57662
person5555 <danmartynovich@gmail.com>
2024-10-23 21:29:49 -0400

    Changes made to get_synergy_boosted_multiplier method comment

--- src/powerup.cr
+++ src/powerup.cr
@@ -54,8 +54,10 @@ class Powerup
     # Will get called after the player's Time Units are updated.
   end
 
-  # base_multiplier assumes boost in addition to base generation. So if multiplier is 5%, base_multiplier should be 0.05
-  # returns in the same format (ex. 0.05 for a 5% boost) 
+  # base_multiplier assumes that only the value expected to be adjusted is entered.
+  # ex1. For a 2x boost, base_multiplier = 2
+  # ex2. For a 5% addition to current units/s, or 1.05x, base_multiplier = 0.05 (since only the 5% change should be affected).
+  # Returns the new multiplier value after having Synergy Matrix boost applied.
   def get_synergy_boosted_multiplier(public_key : String, base_multiplier : Float64) : Float64
     synergy_boost = PowerupSynergyMatrix.get_boost_multiplier(@game, public_key, self.class.get_powerup_id)
     (base_multiplier) * synergy_boost
