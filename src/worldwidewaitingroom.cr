93de64877cb1e177e41236df48411a1327505d3c
Frankie Primerano <frankieprimerano@gmail.com>
2024-10-31 01:20:28 -0400

    idk

--- src/worldwidewaitingroom.cr
+++ src/worldwidewaitingroom.cr
@@ -23,7 +23,7 @@ require "./powerups/breach"
 require "./powerups/signal_jammer"
 require "./powerups/automation_upgrade.cr"
 require "./templates"
-require "./powerups/schrödinger"
+require "./powerups/schrodinger"
 
 alias Secret = String
 alias Public = String
@@ -169,7 +169,7 @@ class Game
       AfflictPowerupSignalJammer.get_powerup_id => AfflictPowerupSignalJammer.new(self),
       AfflictPowerupBreach.get_powerup_id => AfflictPowerupBreach.new(self),
       PowerupAutomationUpgrade.get_powerup_id => PowerupAutomationUpgrade.new(self),
-      PowerupSchrödinger.get_powerup_id => PowerupSchrödinger.new(self),
+      PowerupSchrodinger.get_powerup_id => PowerupSchrodinger.new(self),
     }
   end
 
