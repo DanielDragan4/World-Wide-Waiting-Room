4cac2a84e3b510f96a2a6fb6c152e72416f54731
DanielDragan4 <daniel.dragan4000@gmail.com>
2025-01-08 16:02:55 -0500

    blackhole improvements

--- src/powerups/afflict_black_hole.cr
+++ src/powerups/afflict_black_hole.cr
@@ -2,9 +2,9 @@ require "../powerup"
 require "./force_field.cr"
 
 class AfflictPowerupBlackHole < Powerup
-  STEAL_AMOUNTS = [0.05, 0.125, 0.25, 0.5]
+  STEAL_AMOUNTS = [0.5, 0.25, 0.125, 0.05]
   KEY_DISTANCE = "black_hole_dist"
-  KEY_BIGGEST_DEC = "black_hole_dec"
+  
 
   def get_name
     "Black Hole"
@@ -18,34 +18,47 @@ class AfflictPowerupBlackHole < Powerup
     true
   end
 
-#   def buy_action (public_key)
-#     @game.add_powerup public_key, AfflictPowerupBlackHole.get_powerup_id
-#     @game.set_timer public_key, COOLDOWN_KEY, (get_cooldown_time public_key).to_i64
-#   end
+  def dec_amount(public_key)
+    players_left_and_right = @game.get_black_hole_players public_key
+
+    left = players_left_and_right[0]
+    right = players_left_and_right[1]
+    black_hole_found = false
+
+    i = 0
+    while !black_hole_found
+        if (i < left.size) && !(left.empty?)
+            puts "#{public_key} searching for blackhole #{left[i]} \n"
+            if (@game.has_powerup left[i], PowerupBlackHole.get_powerup_id)
+                black_hole_found = true
+                puts "#{left[i]} owns blackhole"
+                return BigFloat.new(STEAL_AMOUNTS[i])
+            end
+        end
+        if (i < right.size) && !(right.empty?)
+            if (@game.has_powerup right[i], PowerupBlackHole.get_powerup_id)
+                black_hole_found = true
+                return BigFloat.new(STEAL_AMOUNTS[i])
+            end
+        end
+         i += 1
+    end
+    return BigFloat.new(0.0)
+  end
+
+
 
   def action (public_key, dt)
-    if (@game.has_powerup public_key, PowerupForceField.get_powerup_id) #|| (@game.has_powerup public_key, AfflictPowerupBlackHole.get_powerup_id)
+    if (@game.has_powerup public_key, PowerupForceField.get_powerup_id)
       return
     end
 
-    percent_dec = @game.get_key_value_as_int public_key, KEY_DISTANCE
-    biggest_dec = @game.get_key_value_as_int public_key, KEY_BIGGEST_DEC
-
-    if !(biggest_dec.nil?) || (biggest_dec == 0)
-        if biggest_dec < percent_dec
-            @game.set_key_value public_key, KEY_BIGGEST_DEC, percent_dec.to_s
-            biggest_dec = percent_dec
-        end
-    else
-        @game.set_key_value public_key, KEY_BIGGEST_DEC, percent_dec.to_s
-    end
-
-
-
     player_tups = @game.get_player_time_units_ps public_key
-    dec = BigFloat.new(STEAL_AMOUNTS[biggest_dec-1])
+    dec = dec_amount(public_key)
     amount_dec = player_tups * dec
-    puts "#{amount_dec} FROM LEFT #{public_key} (#{@game.get_player_name public_key}) who has #{player_tups}"
+
+    # puts "#{amount_dec} FROM LEFT #{public_key} (#{@game.get_player_name public_key}) who has #{player_tups}"
+
     @game.send_animation_event public_key, Animation::NUMBER_FLOAT, { "value" => "black_hole -#{amount_dec.round(2)}", "color" => "#df1700" }
 
     @game.inc_time_units_ps public_key, -amount_dec
--- src/powerups/blackhole.cr
+++ src/powerups/blackhole.cr
@@ -5,18 +5,16 @@ require "./afflict_black_hole.cr"
 require "math"
 
 class PowerupBlackHole < Powerup
-  BASE_PRICE = BigFloat.new 100_000
+  BASE_PRICE = BigFloat.new 1#00_000
   NEXT_TAKE_COOLDOWN = 1
   DURATION = 60 * 10
-
+  
   PRICE_MULTIPLIER = BigFloat.new 1.3
   KEY_DURATION = "black_hole_duration"
   KEY_NEXT_TAKE_COOLDOWN = "black_hole_next_take"
   KEY_ACTIVE_STACK = "black_hole_active"
-  KEY_DISTANCE = "black_hole_dist"
-  KEY_PLAYERS = "black_hole_players"
-  KEY_BIGGEST_DEC = "black_hole_dec"
-
+  KEY_COOLDOWN = "black_hole_cooldown"
+  COOLDOWN_DURATION = 12 * 60 *60
 
   def category
     PowerupCategory::SABATOGE
@@ -41,7 +39,7 @@ class PowerupBlackHole < Powerup
   end
 
   def get_description(public_key)
-    "Over the course of #{(DURATION / 60).to_i} minutes, steal units from players directly ahead and behind you. For players with less than 1000x your units, steals 20% of their units. For players with 1000x or more units, steals based on 10 raised to 1/4 of the logarithmic difference in units. The price increases with each purchase. Multiple black_holes can be stacked to increase the steal amount."
+    "Over the course of #{(DURATION / 60).to_i} minutes, Reduce the unit generation of the four people ahead and behind you. The effect becomes less powerful the further away the player is from you. "
   end
 
   def get_price(public_key)
@@ -51,7 +49,8 @@ class PowerupBlackHole < Powerup
   end
 
   def is_available_for_purchase(public_key)
-    (((@game.get_player_time_units public_key) >= (get_price public_key)) && (cooldown_seconds_left public_key) <= 0) && !(@game.has_powerup(public_key, PowerupBlackHole.get_powerup_id)) 
+    timer = (@game.get_key_value_as_int public_key, KEY_COOLDOWN) - @game.ts
+    (((@game.get_player_time_units public_key) >= (get_price public_key)) && (cooldown_seconds_left public_key) <= 0) && !(@game.has_powerup(public_key, PowerupBlackHole.get_powerup_id)) && (timer <= 0)
   end
 
   def get_active_black_hole_stack(public_key)
@@ -85,46 +84,36 @@ class PowerupBlackHole < Powerup
     if @game.is_timer_expired public_key, KEY_DURATION
       @game.set_timer public_key, KEY_DURATION, DURATION
     end
-
+    @game.set_key_value public_key, KEY_COOLDOWN, COOLDOWN_DURATION.to_s
     @game.set_timer public_key, KEY_NEXT_TAKE_COOLDOWN, NEXT_TAKE_COOLDOWN
     @game.inc_powerup_stack public_key, PowerupBlackHole.get_powerup_id
   end
-
 
   def action(public_key, dt)
     if !(@game.is_timer_expired public_key, KEY_DURATION) && (@game.is_timer_expired public_key, KEY_NEXT_TAKE_COOLDOWN)
       puts "black_hole action for #{public_key}"
       players_left_and_right = @game.get_black_hole_players public_key
-      current_units = @game.get_player_time_units public_key
-
-      @game.set_key_value public_key, KEY_PLAYERS, players_left_and_right.to_json
 
       left = players_left_and_right[0]
       right = players_left_and_right[1]
 
       puts "#{public_key} LEFT #{left} RIGHT #{right}"
 
-      total = BigFloat.new(0)
-
       if left
-        i = 4
+        i = 3
         left.each do |x|
             if x && x != public_key && !@game.has_powerup x, PowerupForceField.get_powerup_id
                 @game.add_powerup x, AfflictPowerupBlackHole.get_powerup_id
-                @game.set_key_value x, KEY_DISTANCE, i.to_s
-
                 i -= 1
             end
         end
       end
 
       if right
-        i = 4
+        i = 3
         right.each do |x|
             if x && x != public_key && !@game.has_powerup x, PowerupForceField.get_powerup_id
                 @game.add_powerup x, AfflictPowerupBlackHole.get_powerup_id
-                @game.set_key_value x, KEY_DISTANCE, i.to_s
-
                 i -= 1
             end
         end
@@ -135,19 +124,6 @@ class PowerupBlackHole < Powerup
 
   def cleanup(public_key)
     if @game.is_timer_expired public_key, KEY_DURATION
-
-    players = Tuple(Array(String) | Nil, Array(String) | Nil)
-        
-      players = Tuple(Array(String) | Nil, Array(String) | Nil).from_json(@game.get_key_value public_key, KEY_PLAYERS)
-      players.each do |left_or_right|
-        if !(left_or_right.nil?)
-            left_or_right.each do |player|
-                @game.set_key_value player, KEY_BIGGEST_DEC, "0"
-            end
-        end
-    end
-
-
       puts "black_hole expired for #{public_key}"
       reset_active_black_hole_stack public_key
       @game.remove_powerup public_key, PowerupBlackHole.get_powerup_id
